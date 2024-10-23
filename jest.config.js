c7c74c06bcf13bed4722bc4ff81987c654f7c1d4
Max <maxrwal@gmail.com>
2024-10-23 15:30:18 +0100

    tests work while hostable

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,22 @@
 // jest.config.js
+
 module.exports = {
-    testEnvironment: "jsdom", // Simulate a browser environment
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-    moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/$1",
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@data/(.*)$': '<rootDir>/data/$1'
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript and JSX
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react-jsx', // Tell ts-jest to transform JSX for tests
+      },
     },
-    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-    },
-  };
-  
+  },
+};
